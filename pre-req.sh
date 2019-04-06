96055992d2fcc4aaa904485a90555498348794a7
Mehraj Patel <mehrajpatel13@gmail.com>
2019-04-06 14:09:51 +0530

    Update pre-req.sh

--- pre-req.sh
+++ pre-req.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
- wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
+yum install wget -y
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
 for i in {2..3}
 do
  scp jdk-8u131-linux-x64.rpm patel@node$i:~/ 
