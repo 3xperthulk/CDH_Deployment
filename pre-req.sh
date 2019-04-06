0b832073866671bb5e4a1a4b174c7da7352e7310
Mehraj Patel <mehrajpatel13@gmail.com>
2019-04-06 13:59:33 +0530

    Update pre-req.sh

--- pre-req.sh
+++ pre-req.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+ wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
+for i {2..3}
+do
+ scp jdk-8u131-linux-x64.rpm patel@node$i:~/ 
+done
+
+for i {1..3}
+ do
+ ssh patel@node$i "sudo rpm -i jdk-8u131-linux-x64.rpm"
+ done
+ 
 chkconfig iptables off
 chkconfig ip6tables off
 chkconfig ntpd on
