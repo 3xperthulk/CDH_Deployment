02e20a43c9eed9f53922465e6e576d9161235b39
Mehraj Patel <mehrajpatel13@gmail.com>
2019-04-18 21:17:15 +0530

    Update pls.sh

--- pls.sh
+++ pls.sh
@@ -23,3 +23,16 @@ do
  #ssh patel@node$i 'bash -s' < pre-req.sh   #option 1
  ssh patel@node$i 'sudo bash pre-req.sh'    #option 2
 done
+
+sudo yum install wget -y
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
+
+for i in {2..3}
+do
+ scp jdk-8u131-linux-x64.rpm patel@node$i:~/ 
+done
+
+for i in {1..3}
+do
+ ssh patel@node$i "sudo rpm -ivh ~/jdk-8u131-linux-x64.rpm"
+done
