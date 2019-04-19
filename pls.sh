ff78604f8706b4eb1a5ee613d31e01001be6e1fc
Mehraj Patel <mehrajpatel13@gmail.com>
2019-04-19 21:42:06 +0530

    Update pls.sh

--- pls.sh
+++ pls.sh
@@ -28,22 +28,23 @@ done
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
 
 #get mysql
-wget https://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-server-5.6.43-1.el7.x86_64.rpm
-wget https://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-client-5.6.43-1.el7.x86_64.rpm
+#wget https://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-server-5.6.43-1.el7.x86_64.rpm
+#wget https://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-client-5.6.43-1.el7.x86_64.rpm
+sudo yum install http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm -y
+sudo yum install mysql-community-server -y
+sudo systemctl start mysqld
+sudo systemctl enable mysqld.service
 
 
 for i in {2..3}
 do
- scp jdk-8u131-linux-x64.rpm MySQL-server-5.6.43-1.el7.x86_64.rpm MySQL-client-5.6.43-1.el7.x86_64.rpm patel@node$i:~/ 
+ scp jdk-8u131-linux-x64.rpm patel@node$i:~/ 
 done
 
 for i in {1..3}
 do
  ssh patel@node$i "sudo rpm -ivh ~/jdk-8u131-linux-x64.rpm"
- ssh patel@node$i "sudo rpm -ivh ~/MySQL-server-5.6.43-1.el7.x86_64.rpm"
- ssh patel@node$i "sudo rpm -ivh ~/MySQL-client-5.6.43-1.el7.x86_64.rpm" 
- 
-sudo su -c "cat >>/etc/profile.d/java.sh <<EOL
+ sudo su -c "cat >>/etc/profile.d/java.sh <<EOL
 			JAVA_HOME='/usr/java/latest'
 			EOL"
 source 	/etc/profile.d/java.sh
