772ca9e0ee6b3ff4c1f93613dc0dd9a9a1dbfb74
Mehraj Patel <mehrajpatel13@gmail.com>
2019-04-19 21:11:46 +0530

    Update pls.sh

--- pls.sh
+++ pls.sh
@@ -24,7 +24,7 @@ do
  ssh patel@node$i 'sudo bash pre-req.sh'    #option 2
 done
 
-sudo yum install wget -y
+#sudo yum install wget -y
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
 
 #get mysql
@@ -43,8 +43,8 @@ do
  ssh patel@node$i "sudo rpm -ivh ~/MySQL-server-5.6.43-1.el7.x86_64.rpm"
  ssh patel@node$i "sudo rpm -ivh ~/MySQL-client-5.6.43-1.el7.x86_64.rpm" 
  
- sudo su -c 'cat >>/etc/profile.d/java.sh <<EOL
-			JAVA_HOME="/usr/java/latest"
-			EOL'
-source /etc/profile.d/java.sh
+sudo su -c "cat >>/etc/profile.d/java.sh <<EOL
+			JAVA_HOME='/usr/java/latest'
+			EOL"
+source 	/etc/profile.d/java.sh
 done
