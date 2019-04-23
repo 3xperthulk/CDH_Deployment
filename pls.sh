958de39da8dea9b8c4578da96c5da8a78442a090
Mehraj Patel <mehrajpatel13@gmail.com>
2019-04-23 12:32:09 +0530

    Update pls.sh

--- pls.sh
+++ pls.sh
@@ -19,7 +19,7 @@ sudo rm ~/authorized_keys
 sudo rm key.pem
 
 #sudo yum install wget -y
-wget  -O $HOME/ --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
+wget  -O $HOME/jdk-8u131-linux-x64.rpm --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
 
 #get mysql
 #wget https://dev.mysql.com/get/Downloads/MySQL-5.6/MySQL-server-5.6.43-1.el7.x86_64.rpm
