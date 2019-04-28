387e814f92a0bd8925b9593224ed90485c9fe410
Mehraj Patel <mehrajpatel13@gmail.com>
2019-04-28 19:31:57 +0530

    Update cdh-deployment.sh

--- cdh-deployment.sh
+++ cdh-deployment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sudo yum install wget -y
 sudo wget https://archive.cloudera.com/cm5/installer/latest/cloudera-manager-installer.bin
-
+sudo chmod a+x cloudera-manager-installer.bin
 sudo ./cloudera-manager-installer.bin
 sudo systemctl restart cloudera-scm-server
 sudo systemctl enable cloudera-scm-server
