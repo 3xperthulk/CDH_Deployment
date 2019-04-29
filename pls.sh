d19b8fcdf8941f49b5c39e0f76f00347813704e6
Mehraj Patel <mehrajpatel13@gmail.com>
2019-04-29 23:31:34 +0530

    Update pls.sh

--- pls.sh
+++ pls.sh
@@ -22,6 +22,13 @@ done
 sudo rm ~/authorized_keys
 sudo rm key.pem
 
+sudo wget  -O $HOME/jdk-8u131-linux-x64.rpm --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
+
+for i in {2..3}
+do
+ scp jdk-8u131-linux-x64.rpm $username@node$i:~/ 
+done
+
 for ((i=3; i>=1; i--))
 do 
  ssh $username@node$i 'sudo bash pre-req.sh'  
