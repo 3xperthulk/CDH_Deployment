f4e93b43f92a740ebaa9dff4a38cb14edfb79530
Mehraj Patel <mehrajpatel13@gmail.com>
2019-05-21 11:16:36 +0530

    Update pls.sh

--- pls.sh
+++ pls.sh
@@ -17,12 +17,13 @@ done
 for i in `cat $HOME/hosts`
 do
  scp -i key.pem authorized_keys $username@$i:$HOME/.ssh/authorized_keys
- scp -i key.pem pre-req.sh $username@$i:$HOME/ 
+ scp -i key.pem $HOME/Hadoop-Configurations/pre-req.sh $username@$i:$HOME/ 
  ssh $username@$i 'chmod a+x pre-req.sh'
 done
 
 sudo rm ~/authorized_keys
-sudo rm key.pem
+sudo rm $HOME/Hadoop-Configurations/key.pem
+sudo rm $HOME/pre-req.sh
 
 sudo wget  -O $HOME/jdk-8u131-linux-x64.rpm --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
 
@@ -34,7 +35,7 @@ done
 for i in `cat $HOME/hosts`
 do
  ssh $username@$i "sudo rpm -ivh ~/jdk-8u131-linux-x64.rpm"
- ssh $username@$i "sudo cat 'echo "JAVA_HOME=/usr/java/latest" | sudo tee -a /etc/profile.d/java.sh"
+ ssh $username@$i "echo 'JAVA_HOME=/usr/java/latest' | sudo tee -a /etc/profile.d/java.sh"
  ssh $username@$i "source /etc/profile.d/java.sh"
 done
 
